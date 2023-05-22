d6d7ed24734d90a98b693512f58a25d9cf45f253
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-05-22 16:20:35 +0600

    email section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../../../shared/Banner/Banner';
 import Gallery from '../../../shared/Gallery/Gallery';
+import Store from '../../../shared/StoreSection/Store';
 import Subcategory from '../../../shared/SubCategory/Subcategory';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Gallery></Gallery>
             <Subcategory></Subcategory>
+            <Store></Store>
         </div>
     );
 };
---
+++ src/shared/StoreSection/Store.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Store = () => {
+    return (
+        <div className='grid lg:grid-cols-2'>
+            <div className='text-center bg-[#ec6f66] text-white p-10'>
+                <h1 className='font-bold text-xl'>Our store</h1>
+                <p>Pzza Martiri Olivetta, 16, 16034, Portofino, Italy</p>
+                <hr className='w-12 ml-[270px] mt-3 mb-3' />
+                <p className='font-bold'>Opening hours</p>
+                <p><small>Monday to Friday: 9am - 6pm</small></p>
+                <p><small>Saturday to Sunday: 10am - 4pm</small></p>
+            </div>
+            <div className='text-center bg-[#8DC26F]  text-white p-10'>
+                <h1 className='font-bold text-xl'>SIGN UP FOR UPDATES</h1>
+                <p><small>Sign up to get exclusive offers from our store</small></p>
+                <input type="email" name="eemail" placeholder='YOUR EMAIL ADDRESS' id="" className='p-3 mt-4 text-black' />
+                <br />
+                <button className='btn bg-[#4c976b] mt-3'>Subscribe</button>
+            </div>
+        </div>
+    );
+};
+
+export default Store;
